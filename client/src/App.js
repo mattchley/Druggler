b2840c080bf57207f03d5e3dcff4d6815896791d
Jocelyn Chang <jocelync1094@gmail.com>
2020-03-01 21:22:59 -0800

    created a simple signup form that saves the username and password input

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import Header from "./components/Header";
+import SignUp from "./pages/Signup";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
-
+        <SignUp />
         <h2>Welcome to React</h2>
         <p>
           To get started, edit <code>src/App.js</code> and save to reload.
