4edd3285b20f92f281999eb64737d90dbf4307f1
uchrissd <uchrissd@protonmail.com>
2020-03-05 19:52:25 -0800

    created the active drugs components, add drug components, and the add drug page, and the drug modal component

---
+++ client/src/components/AddDrug.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Modal from "../components/DrugModal";
+
+const AddDrug = () => (
+  <div>
+    <h1>Add your drugs here</h1>
+    <br></br>
+    <h2>Here are your drugs</h2>
+    <br></br>
+    <div>
+      <p>Drug 1</p>
+      <Modal />
+    </div>
+    <div>
+      <p>Drug 2</p>
+      <Modal />
+    </div>
+    <div>
+      <p>Drug 3</p>
+      <Modal />
+    </div>
+  </div>
+);
+
+export default AddDrug;
---
+++ client/src/pages/AddDrugPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import AddDrug from "./../components/AddDrug";
+
+const AddDrugPage = () => {
+  return <AddDrug />;
+};
+
+export default AddDrugPage;
