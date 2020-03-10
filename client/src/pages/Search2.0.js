8b0351aa1eedb0289bed27bd99a1944fc8366039
mattchley <mattchley@gmail.com>
2020-03-10 15:15:20 -0700

    cleanedUp

--- client/src/pages/Search2.0.js
+++ client/src/pages/Search2.0.js
@@ -8,11 +8,14 @@ import Button from '@material-ui/core/Button';
 function SearchV2() {
     const [drugs, setDrugs] = useState([]);
     const [search, setSearch] = useState({})
-    const [formInput, setformInput] = useState({})
     const [conflicts, setConflicts] = useState([]);
+    // Tester:
+    // Fluconazole
+    // Bosentan
+    // Simvastatin
 
     const addDrug = () => {
-        setformInput(search)
+        loadDrugs(search)
     }
 
     const fetchConflict = () => {
@@ -28,6 +31,10 @@ function SearchV2() {
                     id: conflicts.length,
                     severity: res.data.fullInteractionTypeGroup[1].fullInteractionType[0].interactionPair[0].severity,
                     details: res.data.fullInteractionTypeGroup[1].fullInteractionType[0].comment
+                    // res.data.fullInteractionTypeGroup[]
+                    // [0]DrugBank[i]
+                    // .fullInteractionType[i].comment (verbose details about the conflict)
+                    // .fullInteractionType[i].interactionPair[i].interactionConcept.description (chemical details)
                 }
             ])
             console.log(res.data)
@@ -35,58 +42,73 @@ function SearchV2() {
             console.log(err)
         });
     }
-// res.data.fullInteractionTypeGroup[]
-// [0]DrugBank[i]
-// .fullInteractionType[i].comment (verbose details about the conflict)
-// .fullInteractionType[i].interactionPair[i].interactionConcept.description (chemical details)
 
-// Fluconazole
-// Bosentan
-// Simvastatin
+    const loadDrugs = (search) => {
+        API.getDrugsID(search)
+            .then(res => {
+                setDrugs([
+                    ...drugs,
+                    {
+                        id: drugs.length,
+                        name: res.data.idGroup.name,
+                        rxcui: res.data.idGroup.rxnormId
+                    }
+                ])
 
-useEffect(() => {
-    axios
-        .get(`https://rxnav.nlm.nih.gov/REST/rxcui?name=${formInput}`)
-        .then(res => {
-            setDrugs([
-                ...drugs,
-                {
-                    id: drugs.length,
-                    name: res.data.idGroup.name,
-                    rxcui: res.data.idGroup.rxnormId
-                }
-            ])
+                console.log(res.data.idGroup.rxnormId)
 
-        }).catch(err => {
-            console.log(err)
-        });
-}, [formInput])
+            })
+            .catch(err => console.log(err));
+    };
 
-return (
-    <div>
+
+    return (
         <div>
-            <TextField type="text" onChange={e => setSearch(e.target.value)}></TextField>
-            <Button type="button" onClick={addDrug}>FETCH DRUG</Button>
-        </div>
+            <div>
+                <TextField type="text" onChange={e => setSearch(e.target.value)}></TextField>
+                <Button type="button" onClick={addDrug}>FETCH DRUG</Button>
+            </div>
 
-        <>
-            <ul>
-                {drugs.map(drug => (
-                    <li key={drug.id}>Name:{drug.name} RXCUI:{drug.rxcui}</li>
-                ))}
-            </ul>
-        </>
-        <Button type="button" onClick={fetchConflict}> Submit for conflicts</Button>
-        <>
-            <ul>
-                {conflicts.map(conflict => (
-                    <li key={conflict.id}>Details: {conflict.details} Severity:{conflict.severity}</li>
-                ))}
-            </ul>
-        </>
-        
-    </div>
-);
+            <div>
+                <ul>
+                    {
+                        drugs.length ? (
+                            <div>
+                                {
+                                    drugs.map(drug => (
+                                        <li key={drug.id}>Name:{drug.name} RXCUI:{drug.rxcui}</li>
+                                    ))
+                                }
+                            </div>
+
+                        ) : (
+                                <h3>No Drugs Added</h3>
+                            )
+                    }
+                </ul>
+            </div>
+            <Button type="button" onClick={fetchConflict}> Submit for conflicts</Button>
+            <div>
+                <ul>
+                    {
+                        conflicts.length ? (
+                            <div>
+                                {
+                                    conflicts.map(conflict => (
+                                        <li key={conflict.id}>Details: {conflict.details} Severity:{conflict.severity}</li>
+                                    ))
+                                }
+                            </div>
+                        ) : (
+                                <h3>No Conflicts to Display</h3>
+                            )
+                    }
+
+                </ul>
+            </div>
+
+        </div>
+    );
 }
 
 export default SearchV2;
