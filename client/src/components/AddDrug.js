f4011e416c87bca872622d62bae9887037c40b28
Mark Bernstein <ProgrammingByMark@gmail.com>
2020-03-12 19:44:35 -0700

    updated

--- client/src/components/AddDrug.js
+++ client/src/components/AddDrug.js
@@ -23,7 +23,10 @@ import { white } from "material-ui/styles/colors";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1
+    flexGrow: 1,
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
   },
   addDrug: {
     padding: theme.spacing(2),
@@ -35,7 +38,7 @@ const useStyles = makeStyles(theme => ({
     fontWeight: "900",
     align: "left"
   },
-  title: {
+  title: {                 
     padding: theme.spacing(2),
     textAlign: "center",
     backgroundColor: "darkgray",
@@ -96,18 +99,18 @@ export default function AddDrug() {
     const loadData = async () => {
       let currentUser = await API.dashboard(Auth.getToken());
       setUser(currentUser.data.user);
-      // console.log(currentUser);
+      console.log(currentUser);
       let currentDrugs = await API.getAllUserDrugs(
         currentUser.data.user._id,
         Auth.getToken()
       );
-      // console.log(currentDrugs);
+      console.log(currentDrugs);
       setAllDrugs(currentDrugs.data);
       let userDrugArray = await API.saveDrugtoUser(
         currentUser.data,
         Auth.getToken()
       );
-      // console.log(userDrugArray);
+      console.log(userDrugArray);
     };
 
     loadData();
@@ -116,19 +119,19 @@ export default function AddDrug() {
   const handleInputChange = e => {
     const { name, value } = e.target;
     setDrugDetails({ ...drugDetails, [name]: value });
-    // console.log(drugDetails);
+    console.log(drugDetails);
   };
 
   const handleFormSubmit = event => {
     event.preventDefault();
-    // console.log(
-    //   drugDetails.lastTaken,
-    //   drugDetails.frequency,
-    //   "HELLO DRUG DETAILS"
-    // );
+    console.log(
+      drugDetails.lastTaken,
+      drugDetails.frequency,
+      "HELLO DRUG DETAILS"
+    );
     if (drugDetails.lastTaken && drugDetails.frequency) {
-      // console.log("before API front end");
-      // console.log("current user, ", user._id);
+      console.log("before API front end");
+      console.log("current user, ", user._id);
       API.saveDrug(
         {
           name: drugDetails.name,
@@ -139,7 +142,7 @@ export default function AddDrug() {
         Auth.getToken()
       )
         .then(res => {
-          // console.log(res);
+          console.log(res);
           setAddedDrug(allDrugs.length + 5);
         })
         .then(res => handleClose())
@@ -177,11 +180,7 @@ export default function AddDrug() {
                   <p className={classes.pillGrid}>Delete?</p>
                 </TableHeaderColumn>
                 <TableHeaderColumn>
-<<<<<<< HEAD
                   <p className={classes.pillGrid}  >Take Pill</p>
-=======
-                  <p className={classes.pillGrid2}  >Take Pill</p>
->>>>>>> bee5e76303e43f2b2c49005a7ea5dd719d25287b
                 </TableHeaderColumn>
               </TableRow>
             </TableHeader>
